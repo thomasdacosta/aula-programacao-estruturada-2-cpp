82eab28562bb04a007a25226e1ab25584185e9c8
Thomás da Costa <thomasdacosta@gmail.com>
2015-03-14 00:59:39 -0300

    #excluindo para colocar a nova estrutura

--- AULA02/matematica.h
+++
@@ -1,7 +0,0 @@
-namespace matematica
-{
-	double somar(double x, double y);
-	double subtrair(double x, double y);
-	double multiplicar(double x, double y);
-	double dividir(double x, double y);
-}
--- aula01/revisao_ola_mundo_2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	cout << "Meu nome é Carlos. \nTenho " << 25 << " anos de idade";
-}
