c60c8862281d10f67c3630870678a76a24f70b16
Thomás da Costa <thomasdacosta@gmail.com>
2015-03-14 00:58:16 -0300

    #nova estrutura de diretorio

---
+++ AULA02/matematica.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "matematica.h"
+
+using namespace std;
+
+namespace matematica
+{
+	double somar(double x, double y)
+	{
+		return x + y;
+	}
+	
+	double subtrair(double x, double y)
+	{
+		return x - y;
+	}
+	
+	double multiplicar(double x, double y)
+	{
+		return x * y;
+	}
+	
+	double dividir(double x, double y)
+	{
+		return x / y;
+	}
+}
